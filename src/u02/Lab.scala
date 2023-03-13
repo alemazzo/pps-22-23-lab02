9d607d9d202626bb6bcf8d9593a7599bdd9f9ee1
Alessandro <alemazzoli97@gmail.com>
2023-03-13 10:49:59 +0100

    Implemented generic neg

--- src/u02/Lab.scala
+++ src/u02/Lab.scala
@@ -16,13 +16,26 @@ object Lab extends App:
   private val valNeg: (String => Boolean) => (String => Boolean) =
     p => !p(_)
 
-  private def neg(predicate: String => Boolean): String => Boolean =
+  private def defNeg(predicate: String => Boolean): String => Boolean =
     !predicate(_)
 
   val emptyString: String => Boolean = _ == ""
 
-  val notEmptyString = neg(emptyString)
+  val notEmptyString = defNeg(emptyString)
 
   println(notEmptyString("ciao"))
   println(notEmptyString(""))
   println(emptyString("ciao"))
+
+  private def neg[A](predicate: A => Boolean): A => Boolean =
+    !predicate(_)
+
+  val even: Int => Boolean = _ % 2 == 0
+  val odd = neg(even)
+
+  println(odd(3))
+  println(odd(4))
+
+
+
+
