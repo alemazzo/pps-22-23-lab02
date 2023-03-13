40a651cb504306959a3171be11ac0bd935dd6ef7
Alessandro <alemazzoli97@gmail.com>
2023-03-13 10:40:51 +0100

    Implemented positive with val and def

--- src/u02/Lab.scala
+++ src/u02/Lab.scala
@@ -1,4 +1,14 @@
 package u02
 
 object Lab extends App:
-  
+
+  private val valPositive: Int => Boolean = _ match
+    case n if n >= 0 => true
+    case _ => false
+
+  private def positive(x: Int): Boolean = x match
+    case n if n >= 0 => true
+    case _ => false
+
+  println(positive(2))
+  println(positive(-2))
